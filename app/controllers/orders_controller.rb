a0cb30deddadd4ae10d4496aed9c8ad758cc9d74
HORII Keima <holysugar@gmail.com>
2017-10-05 16:43:30 +0900

    wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
-  def show
-    redirect_to Order.new
+  def index
+    redirect_to new_order_url
   end
 
   def new
@@ -18,16 +18,6 @@ class OrdersController < ApplicationController
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :bookshelves
+  root to: "welcome#index"
   resources :books
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :orders
+
+  resources :orders, only: [:index, :new, :create]
 end
